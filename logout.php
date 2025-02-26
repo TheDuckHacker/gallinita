668cf2e99d419e3eb4a2c93feefd644f973db332
TheDuckHacker <carlaloayza06@gmail.com>
2025-02-25 21:53:25 -0400

    Delete logout.php

--- logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_unset(); // Eliminar todas las variables de sesión
-session_destroy(); // Destruir la sesión
-header("Location: IniciodeSesion.php"); // Redirigir al inicio de sesión
-exit();
-?>
